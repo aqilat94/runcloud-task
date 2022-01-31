a7aa3f1aee0a9f72de030dc554d409ae0c0b845b
aqilat94 <aqilat94@gmail.com>
2022-01-31 13:38:49 +0800

    register policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,7 +13,8 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        'App\Models\Workspace' => 'App\Policies\WorkspacePolicy',
+        'App\Models\Task' => 'App\Policies\TaskPolicy',
     ];
 
     /**
