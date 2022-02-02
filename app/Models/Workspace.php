1d57b4a25376024bdbb1c53f16c22d79b80281de
aqilat94 <aqilat94@gmail.com>
2022-02-03 00:12:25 +0800

    remove softdelete at workspace

--- app/Models/Workspace.php
+++ app/Models/Workspace.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Workspace extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $fillable = [
         'name',
